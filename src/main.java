f7171385ca467621698ba8bfffb68f88c6834a61
krishnatkocherla <krishnatkocherla@gmail.com>
2025-11-07 21:26:41 -0500

    fixed two code smells in main

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class main {
+public class Main {
 	public static void main(String[] args) {
         Item item1 = new Item("Book", 20, 1, DiscountType.AMOUNT, 5);
         Item item2 = new TaxableItem("Laptop", 1000, 1, DiscountType.PERCENTAGE, 0.1);
